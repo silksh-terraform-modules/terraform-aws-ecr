38bcf78405a61b285f90a0a0fe7d3ee0e65fe781
Tomasz Warczykowski <tomasz.warczykowski@silksh.pl>
2026-01-19 12:18:10 +0100

    add force_delete option

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ecr_repository" "this" {
   name = var.repository_name
+  force_delete = var.force_delete
 }
 
 resource "aws_ecr_lifecycle_policy" "this" {
--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "repository_name" {
   default = ""
   description = "repository name"
 }
+
+variable "force_delete" {
+  default = false
+  type = bool
+  description = "allow to delete repository even if there are images"
+}
